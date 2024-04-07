e9be75084442199103dd36c37430461d93f5efed
harirajv <harirajv@gmail.com>
2024-04-07 15:13:05 -0700

    Initial structure

---
+++ src/test/java/com/example/BankDemo/BankDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
